1284d18e20d031a2c11563f76fc083ddcf07308d
DanSoup <dansoup@hotmail.co.uk>
2019-11-03 14:51:00 +0000

    set up the first part of the server and the extracting of the parameters and queries

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+
+  // console.log(req.params)
+  console.log(Object.keys(req))
+  console.log(req.url)
+
+  const [parametersString, queriesString = ''] = req.url.split('?');
+
+  const parameters = parametersString.split('/').slice(1);
+  const queries = queriesString.split('&').reduce((acc, query) => {
+    const [key, value] = query.split('=');
+    acc[key] = value;
+    return acc;
+  }, {})
+
+  console.log(parameters)
+  console.log(queries)
+
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify(require('./data/projects/projects.json')));
+  res.end();
+
+}).listen(8080);
